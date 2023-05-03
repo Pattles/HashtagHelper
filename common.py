8f2ccbf17bc0894bb3af26e8761ee805e049ddd1
Pattles <doomrock8@gmail.com>
2023-05-03 17:34:05 -0400

    revamped caption process for multi-word keywords

--- common.py
+++ common.py
@@ -2,12 +2,6 @@ from flask import Flask, request, render_template, jsonify
 import json
 import requests
 import sqlite3
+from flashtext import KeywordProcessor
 
 google_maps_api_key = 'AIzaSyA3BVGehNIwyoFXC_mYgrOnntTbtMuzp20'
-
-# MongoDB Credentials
-"""
-Database User Username: Cluster83411
-Database User Password: UEZKb19Ebkx8
-Your connection string: mongodb+srv://cluster83411.cuazoek.mongodb.net
-"""
